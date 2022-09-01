055286055bb2dfc9f500b475fdb14705d2f98175
zaffar-maqbool <aalimwani94@gmail.com>
2022-09-01 15:30:28 +0530

    modified:   conditionals/conditionals.go

--- conditionals/conditionals.go
+++ conditionals/conditionals.go
@@ -11,10 +11,12 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Please input your age ")
 	userInput, _ := reader.ReadString('\n')
-	userAge, _ := strconv.ParseInt(userAgeinput, 0, 64)
+	userAge, _ := strconv.ParseInt(userInput, 0, 64)
 
 	if userAge >= 18 {
 		fmt.Println("Welcome to club")
+	} else {
+		fmt.Println("Sorry YU Cant join")
 	}
 
 }
