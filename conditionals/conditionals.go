82ad301d94693612e73426148c59ce6992a9ca92
zaffar-maqbool <aalimwani94@gmail.com>
2022-09-02 12:08:25 +0530

    modified:   conditionals/conditionals.go modified:   conditionals/conditionals.go

--- conditionals/conditionals.go
+++ conditionals/conditionals.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
 
-	// reader := bufio.NewReader(os.Stdin)
-	// fmt.Println("Please input your age ")
-	// userInput, _ := reader.ReadString('\n')
-	// userAge, _ := strconv.ParseInt(userInput, 0, 64)
-
-	userAge := 30
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please input your age ")
+	userInput, _ := reader.ReadString('\n')
+	userAge, err := strconv.ParseInt(userInput, 0, 64)
+	fmt.Println(err)
+	if err != nil {
+		fmt.Println("invalid input")
+	}
+	//userAge := 30
 	//isOldEnough := userAge >= 18
 	if userAge >= 30 && userAge < 50 || userAge >= 60 {
 		fmt.Println("Welcome to club")
