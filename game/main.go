fc92cc3f065526275b54c8dd4bbe89acc0a6fd3b
zaffar-maqbool <aalimwani94@gmail.com>
2022-09-03 13:01:22 +0530

    new file:   game/interactions/output.go 	modified:   game/main.go

--- game/main.go
+++ game/main.go
@@ -1,5 +1,24 @@
 package main
 
+func startGme() {
+
+}
+func executeRound() string {
+
+}
+func endGame() {
+
+}
+
 func main() {
 
+	startGme()
+
+	winner := "" // "Player" || "Monster" || ""
+
+	for winner == "" {
+		winner = executeRound()
+	}
+
+	endGame()
 }
