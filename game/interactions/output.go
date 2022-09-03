2527a76fbb0083622075c58da7f4379a86f42899
zaffar-maqbool <aalimwani94@gmail.com>
2022-09-03 13:09:28 +0530

    modified:   game/interactions/output.go 	modified:   game/main.go

--- game/interactions/output.go
+++ game/interactions/output.go
@@ -1,5 +1,11 @@
 package interactions
 
-func PrintGreeting() {
+import (
+	"fmt"
+)
 
+func PrintGreeting() {
+	fmt.Println("MONSTER SLAYER")
+	fmt.Println("Starting new game ...")
+	fmt.Println("Good Luck:")
 }
--- game/main.go
+++ game/main.go
@@ -1,10 +1,22 @@
 package main
 
-func startGme() {
+import (
+	"fmt"
 
+	"github.com/zaffar-maqbool/Golang/game/interactions"
+)
+
+var currentRound = 0
+
+func startGme() {
+	interactions.PrintGreeting()
 }
 func executeRound() string {
+	currentRound++
+	isSpecialRound := currentRound%3 == 0
+	//to know wheather speacial attack can be used
 
+	fmt.Println("")
 }
 func endGame() {
 
