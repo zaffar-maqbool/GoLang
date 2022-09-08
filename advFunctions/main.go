6b6dc94ef8467fc849b461e69c5efdcc055f4407
zaffar-maqbool <aalimwani94@gmail.com>
2022-09-08 14:45:21 +0530

    modified:   advFunctions/main.go

--- advFunctions/main.go
+++ advFunctions/main.go
@@ -5,32 +5,41 @@ import "fmt"
 func double(number int) int {
 	return number * 2
 }
-func doubleNumbers(numbers *[]int) []int {
-	dNumbers := []int{}
 
-	for _, val := range *numbers {
-		dNumbers = append(dNumbers, double(val))
-	}
-	return dNumbers
-}
+// func doubleNumbers(numbers *[]int) []int {
+// 	dNumbers := []int{}
+
+// 	for _, val := range *numbers {
+// 		dNumbers = append(dNumbers, double(val))
+// 	}
+// 	return dNumbers
+// }
 
 func triple(number int) int {
 	return number * 3
 }
-func tripleNumbers(numbers *[]int) []int {
-	dNumbers := []int{}
+
+// func tripleNumbers(numbers *[]int) []int {
+// 	dNumbers := []int{}
+
+// 	for _, val := range *numbers {
+// 		dNumbers = append(dNumbers, triple(val))
+// 	}
+// 	return dNumbers
+// }
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
+	transNumbers := []int{}
 
 	for _, val := range *numbers {
-		dNumbers = append(dNumbers, triple(val))
+		transNumbers = append(transNumbers, transform(val))
 	}
-	return dNumbers
+	return transNumbers
 }
-
 func main() {
 	numbers := []int{1, 2, 3, 4}
-	doubled := doubleNumbers(&numbers)
+	doubled := transformNumbers(&numbers, double)
 
 	fmt.Println(doubled)
-	tripled := tripleNumbers(&numbers)
+	tripled := transformNumbers(&numbers, triple)
 	fmt.Println(tripled)
 }
