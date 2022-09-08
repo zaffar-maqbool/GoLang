4cded2d9dae9b2561d21681d9fe5034c8faa6681
zaffar-maqbool <aalimwani94@gmail.com>
2022-09-08 13:56:21 +0530

    modified:   advValue/main.go

--- advValue/main.go
+++ advValue/main.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
+type person struct {
+	name string
+	age  int
+}
+
+type customNumber int
+
+type personData map[string]person
+
+// func (number int) pow() {
+// 	return number * 2
+// }
+
+func (number customNumber) pow(power int) customNumber {
+	result := number
+
+	for i := 1; i < power; i++ {
+		result = result * number
+	}
+	return result
+}
+
 func main() {
 	//hobbies := []string{"Cook", "Code"}
 	hobbies := make([]string, 2, 10)
@@ -17,4 +39,15 @@ func main() {
 	*moreHobbies = append(*moreHobbies, "WORK")
 	fmt.Println(*moreHobbies)
 
+	var people []person = []person{
+		{"Max", 32},
+	}
+	fmt.Println(people)
+	var peps personData = personData{
+		"p1": {"ZAFFAR", 22},
+	}
+	fmt.Println(peps)
+	var dummyNumber customNumber = 5
+	poweredNumber := dummyNumber.pow(3)
+	fmt.Println(poweredNumber)
 }
