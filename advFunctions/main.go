8e94bf04c6dfda0934b57781cf70c08958a83195
zaffar-maqbool <aalimwani94@gmail.com>
2022-09-09 13:49:58 +0530

    modified:   advFunctions/main.go

--- advFunctions/main.go
+++ advFunctions/main.go
@@ -8,21 +8,19 @@ type transformFn func(int) int
 
 func main() {
 	numbers := []int{1, 2, 3, 4}
-	moreNumbers := []int{5, 1, 2}
+
+	double := createTransformer(2)
+	triple := createTransformer(4)
+
+	transformed := transformNumbers(&numbers, func(number int) int {
+		return number * 2
+	})
 	doubled := transformNumbers(&numbers, double)
 	tripled := transformNumbers(&numbers, triple)
 
+	fmt.Println(transformed)
 	fmt.Println(doubled)
 	fmt.Println(tripled)
-
-	transformerFn1 := getTransformerFunction(&numbers)
-	transformerFn2 := getTransformerFunction(&moreNumbers)
-
-	transformedNumbers := transformNumbers(&numbers, transformerFn1)
-	moreTransformedNumbers := transformNumbers(&moreNumbers, transformerFn2)
-
-	fmt.Println(transformedNumbers)
-	fmt.Println(moreTransformedNumbers)
 }
 
 func transformNumbers(numbers *[]int, transform transformFn) []int {
@@ -34,19 +32,9 @@ func transformNumbers(numbers *[]int, transform transformFn) []int {
 
 	return dNumbers
 }
-
-func getTransformerFunction(numbers *[]int) transformFn {
-	if (*numbers)[0] == 1 {
-		return double
-	} else {
-		return triple
+func createTransformer(factor int) func(int) int {
+	return func(number int) int {
+		return number * factor
 	}
-}
 
-func double(number int) int {
-	return number * 2
-}
-
-func triple(number int) int {
-	return number * 3
 }
